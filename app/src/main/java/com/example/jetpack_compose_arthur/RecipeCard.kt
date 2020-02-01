b757be3d6244e20de081afaea89a9f911078a7d9
adioses <arthur.felipe.dioses.reto@everis.com>
2020-02-01 16:09:14 -0500

    Imp() - Se agregan imagen al RecipeCard - Se agrega libreria de foundation para mostrar el ImageDraw

--- app/src/main/java/com/example/jetpack_compose_arthur/RecipeCard.kt
+++ app/src/main/java/com/example/jetpack_compose_arthur/RecipeCard.kt
@@ -1,12 +1,25 @@
 package com.example.jetpack_compose_arthur
 
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
+import androidx.ui.core.dp
+import androidx.ui.foundation.DrawImage
+import androidx.ui.layout.Column
+import androidx.ui.layout.Container
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 
 @Composable
 fun RecipeCard(recipe: Recipe) {
-    Text(recipe.tittle)
+    //Text(recipe.tittle)
+    val image = +imageResource(recipe.imageResource)
+    Column {
+        Container(expanded = true, height = 144.dp) {
+            DrawImage(image = image)
+        }
+        Text(recipe.tittle)
+    }
 }
 
 @Composable
